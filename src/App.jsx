e8a7b01a1b7a5767efb2a76783b123985069b158
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-11-04 09:04:48 +0000

    added hashrouter to enable routing to work on gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home';
 import AboutMe from './pages/AboutMe/AboutMe';
 import Skills from './pages/Skills/Skills';
